f4d785708c17324fe0cebfa4bc97d927d508f41e
Mani <manibhushansingh20@gmail.com>
2020-06-02 10:37:17 +0530

    Changes made in App.cs

--- scart/src/App.js
+++ scart/src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import {Route} from 'react-router-dom'
 import HomePage from './Pages/Home/HomePage'
+import Login from './Pages/Login/Login'
 
 function App() {
   return (
     <div className="App">
-    <Route exact path='/' component={HomePage} />
+    <Route exact path='/' component={Login} />
+    <Route exact path='/Home' component={HomePage} />
     </div>
   );
 }
--- scart/src/Pages/Home/HomePage.js
+++ scart/src/Pages/Home/HomePage.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import './HomePage.css';
 import Productlist from '../../components/ProductList /productList'
-
-
+import SearchBox from '../../components/SearchComponent/SearchBox'
+import Loader from '../../components/Loader/Loader'
 
 class HomePage extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        
+        loading:false
       };
+    }
+
+   async handleSearch(text){
+
+       await console.log(text);
+
     }
   
     render() {
       return (
         <div className="mainContent">
+            {this.state.loading?<div className="loaderLayout">
+            <Loader/>
+             </div>:null
+            }
+          
             <div className="topContent" >
+            <div className="logoLayout">
+            <span>Mani</span>
+            </div>
+
+            <div className="searchLayout">
+            <SearchBox onChange={e => this.handleSearch(e.target.value)}/>
+            </div>
+
+            <div className="cartShowLayput">
             <span>mani</span>
+            </div>
+           
             </div>
             <div className="leftFilterContent">
             <span>BHUSHAN</span>
