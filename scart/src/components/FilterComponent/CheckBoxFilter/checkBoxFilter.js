dc961be39585869b6661e3d94932575caa128c27
Mani <manibhushansingh20@gmail.com>
2020-06-03 11:09:43 +0530

    no message

--- scart/src/components/FilterComponent/CheckBoxFilter/checkBoxFilter.js
+++ scart/src/components/FilterComponent/CheckBoxFilter/checkBoxFilter.js
@@ -6,11 +6,18 @@ import './checkBoxFilter.css'
 function CheckBoxFilter(props)
 {
 
+   
     return(
 
          <div className='checkBoxFilterLyout' >
          <span className='titleStyle' >Color</span>
-         <div>
+         <div className='checkBoxBody' >
+          <input
+            type="checkbox"
+    
+          />
+          <span className='textStyle'>Label Text</span>
+    
          </div>
          </div>
     )
